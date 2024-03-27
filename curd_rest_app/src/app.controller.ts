994f6602d7078ea6913108c2934d53f2e704c7e7
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-03-27 22:26:23 +0530

    user reading and creating part done
    work well fix busgs

--- curd_rest_app/src/app.controller.ts
+++ curd_rest_app/src/app.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.models';
+import { get } from 'http';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
- 
- 
+    //for cerating use post method
+
+      @Post()
+      async createUser(@Body() userDto:User){
+        return this.appService.createUser(userDto)
+      }
+      
+    // read data from 
+      @Get()
+      readUser(){
+        return this.appService.readUser()
+      }
+
+
+
+
+
+
 }
--- curd_rest_app/src/app.service.ts
+++ curd_rest_app/src/app.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { User,UserDocumnet } from './user.models';
 import {Model} from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { promises } from 'dns';
+import { error } from 'console';
 
 @Injectable()
 export class AppService {
@@ -11,4 +13,23 @@ export class AppService {
   ){}
 
   // logic for create user
+    async createUser(user: User): Promise<User> {
+      const newUser = new this.userModel(user);
+      return newUser.save();
+
+    }
+
+
+    //reading the data from database
+    async readUser() {
+      try {
+        const users = await this.userModel.find().exec();
+        return users;
+      } catch (error) {
+        console.error(error);
+        throw error; // Rethrow the error to be handled by the caller
+      }
+    }
+  
+
 }
